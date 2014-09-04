b64abf2f463668858c029b376d4ea41df6823d45
Tosialke <MarcinBestPVP@wp.pl>
2014-09-04 20:35:45 +0800

    Dodano

--- CastleMod/src/net/marcinpsp/cm/Main.java
+++ CastleMod/src/net/marcinpsp/cm/Main.java
@@ -3,5 +3,12 @@ package net.marcinpsp.cm;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
+	
+public static Main plugin= null;
+
+
+
+public static Main getPlugin(){
+return plugin;}
 
 }
